ba2be8762273959ccb47848c034a75a34d17cab7
kle-guen <leguen.kln@gmail.com>
2023-11-13 22:12:40 +0100

    new module, edit form , fix navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule} from '@angular/material/toolbar';
-import { MatIcon, MatIconModule} from '@angular/material/icon'
+import { MatIconModule} from '@angular/material/icon'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
     NavBarComponent,
     PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    PokemonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
--- src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route, Router} from '@angular/router';
 import { Pokemon } from '../pokemon';
-import { POKEMONS } from '../mock-pokemon-list';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -12,16 +12,23 @@ export class DetailPokemonComponent implements OnInit{
   pokemonList :Pokemon[];
   pokemon: Pokemon | undefined;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private pokemonService : PokemonService
+  ) {}
 
   ngOnInit() {
-    this.pokemonList = POKEMONS;
+    this.pokemonList = this.pokemonService.getPokemonList();
     const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
     if (pokemonId)
-      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+      this.pokemon = this.pokemonService.getPokemonById(+pokemonId);
   }
 
   goToPokemonList() {
     this.router.navigate(['/pokemons'])
+  }
+  goToEditPokemon(pokemon: Pokemon) {
+    this.router.navigate(['edit/pokemon' , pokemon.id])
   }
 }
